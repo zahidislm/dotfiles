0e16976d3273573d3ff96674fcab5d89df9247e0
Zahid Islam <dev@zahidislm.com>
2021-04-15 15:31:32 -0400

    [zsh] updated plugins list.

--- zsh/modules/plugins/load.zsh
+++ zsh/modules/plugins/load.zsh
@@ -1,7 +1,6 @@
 ## Themes
 zinit ice depth=1; zinit light romkatv/powerlevel10k
 
-## Utilities
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
 zinit light-mode for \
@@ -10,8 +9,12 @@ zinit light-mode for \
     zinit-zsh/z-a-patch-dl \
     zinit-zsh/z-a-bin-gem-node
 
+## Utilities
+zinit wait lucid light-mode for unixorn/fzf-zsh-plugin
 zinit wait lucid light-mode for MichaelAquilina/zsh-autoswitch-virtualenv
+zinit wait lucid light-mode for yun-cloud/zsh-bd
 
+## Syntax Chain
 zinit wait lucid light-mode for \
  atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
     zdharma/fast-syntax-highlighting \
