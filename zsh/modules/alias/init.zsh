f35baf154ae02531926a881cd0a60b4f24a81a5e
Zahid Islam <dev@zahidislm.com>
2022-10-20 18:27:22 -0400

    updated alias module.
    Signed-off-by: Zahid Islam <dev@zahidislm.com>

--- zsh/modules/alias/init.zsh
+++ zsh/modules/alias/init.zsh
@@ -1,12 +1,11 @@
 ## Clear Linux Package Manager
 
 if (( $+commands[swupd] )); then
-  alias swupdc='sudo swupd clean --all'    # Cleans the cache.
-  alias swupda='sudo swupd bundle-add'      # Installs bundle(s).
-  alias swupdl='swupd bundle-list'              # Lists installed bundles.
-  alias swupdi='swupd info'              # Displays bundle information.
-  alias swupdr='sudo swupd bundle-remove'       # Removes bundle(s).
-  alias swupds='sudo swupd search'            # Searches for a package/bundle.
-  alias swupdu='sudo swupd update'       # Updates bundles.
+  alias pkgc='sudo swupd clean --all'    # Cleans the cache.
+  alias pkga='sudo swupd bundle-add'      # Installs bundle(s).
+  alias pkgls='swupd bundle-list'              # Lists installed bundles.
+  alias pkgh='swupd info'              # Displays bundle information.
+  alias pkgr='sudo swupd bundle-remove'       # Removes bundle(s).
+  alias pkgf='sudo swupd search'            # Searches for a package/bundle.
+  alias pkgu='sudo swupd update'       # Updates bundles.
 fi
-
