8f25ffc218261b03cec038d0c30164100c749b5e
Zahid Islam <dev@zahidislm.com>
2022-11-19 16:05:48 -0500

    Added several system utils & starship prompt

--- zsh/modules/plugins/plugins.zsh
+++ zsh/modules/plugins/plugins.zsh
@@ -1,5 +1,5 @@
-## Themes
-zinit ice depth=1; zinit light romkatv/powerlevel10k
+# Alias for enabling TURBO mode
+alias zturbo="zinit wait lucid light-mode for"
 
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
@@ -10,15 +10,57 @@ zinit light-mode for \
     zdharma-continuum/zinit-annex-rust
 
 ## Utilities
-zinit wait lucid light-mode for MichaelAquilina/zsh-autoswitch-virtualenv
+zturbo MichaelAquilina/zsh-autoswitch-virtualenv
+
+## ZOXIDE
+zturbo \
+    as'command' \
+    from'gh-r' \
+    sbin \
+    atinit'ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay' \
+    atclone'./zoxide init zsh > init.zsh' \
+    atpull'%atclone' src'init.zsh' nocompile'!' \
+        ajeetdsouza/zoxide
+
+## EXA
+zturbo \
+    from'gh-r' \
+    as'program' \
+    sbin'**/exa -> exa' \
+    atclone'cp -vf completions/exa.zsh _exa' \
+        ogham/exa
+
+zturbo \
+  has'exa' \
+    zplugin/zsh-exa
+
+## DSF
+zinit ice wait lucid as"program" pick"bin/git-dsf"
+zturbo z-shell/zsh-diff-so-fancy
+
+## FZF
+zturbo \
+    from'gh-r' \
+    sbin \
+        stedolan/jq
+
+zinit pack"bgn-binary" for fzf
+zturbo Aloxaf/fzf-tab
 
 ## Syntax Chain
-zinit wait lucid light-mode for \
- atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+zturbo \
     zdharma-continuum/fast-syntax-highlighting \
- blockf \
     zsh-users/zsh-history-substring-search \
- atload"zicompinit; zicdreplay" blockf \
-    zsh-users/zsh-completions \
- atload"!_zsh_autosuggest_start" \
-    zsh-users/zsh-autosuggestions
+    atload"_zsh_autosuggest_start" \
+        zsh-users/zsh-autosuggestions \
+    blockf atpull'zinit creinstall -q .' \
+        zsh-users/zsh-completions
+
+## STARSHIP PROMPT
+zturbo \
+    as'program' \
+    from'gh-r' \
+    sbin \
+        starship/starship
+
+eval "$(starship init zsh)"
