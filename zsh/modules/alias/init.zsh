f669102257940b9c13f259f849ca3280c64fe8d1
Zahid Islam <dev@zahidislm.com>
2023-01-26 19:32:56 -0500

    fix(aliases): add fedora compat.

--- zsh/modules/alias/init.zsh
+++ zsh/modules/alias/init.zsh
@@ -1,5 +1,4 @@
 ## Clear Linux Package Manager
-
 if (( $+commands[swupd] )); then
   alias pkgc='sudo swupd clean --all'    # Cleans the cache.
   alias pkga='sudo swupd bundle-add'      # Installs bundle(s).
@@ -8,6 +7,15 @@ if (( $+commands[swupd] )); then
   alias pkgr='sudo swupd bundle-remove'       # Removes bundle(s).
   alias pkgf='sudo swupd search'            # Searches for a package/bundle.
   alias pkgu='sudo swupd update'       # Updates bundles.
+## Fedora's DNF Package Manager
+elif (( $+commands[dnf] )); then
+  alias pkgc='sudo dnf clean all'    # Cleans the cache.
+  alias pkga='sudo dnf install'      # Installs packages(s).
+  alias pkgls='dnf list installed'   # Lists installed bundles.
+  alias pkgh='dnf info'              # Displays package information.
+  alias pkgr='sudo dnf remove'       # Removes package(s).
+  alias pkgf='sudo dnf search'       # Searches for a package/bundle.
+  alias pkgu='sudo dnf upgrade'       # Updates packages.
 fi
 
 ## NEOVIM
@@ -24,4 +32,9 @@ fi
 ## FZF
 if (( $+commands[bat] && $+commands[fzf] )); then
     alias fzfp='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+fi
+
+## Python
+if (( $+commands[python3] )); then
+    alias python='python3'
 fi
