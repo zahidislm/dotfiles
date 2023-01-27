1b8d85505ba0a4eb4dde5e00a4547b0064223fef
Zahid Islam <dev@zahidislm.com>
2023-01-26 19:30:19 -0500

    fix(cli-tools): changed system auto theme selection.
    - use One Half Dark/Light for bat, fzf, and git
    - removed portable ripgrep install (use system instead for now)
    - added vivid tool to add more terminal theming

--- zsh/modules/cli-tools/init.zsh
+++ zsh/modules/cli-tools/init.zsh
@@ -16,12 +16,13 @@ zturbo \
     atclone'cp -vf completions/exa.zsh _exa' \
   ogham/exa
 
-# RIPGREP
+# vivid
 zturbo \
     from'gh-r' \
     as'command' \
-    sbin'**/rg -> rg' \
-  BurntSushi/ripgrep
+    sbin'**/vivid -> vivid' \
+    atload'export LS_COLORS="$(vivid generate one-${${SysTheme:l}:-dark})"' \
+  @sharkdp/vivid
 
 ## BAT
 zturbo \
@@ -33,7 +34,7 @@ zturbo \
 
 # Enable synthax auto-theming support
 local bat_config=$HOME/.config/bat/config
-auto_theme_tool_syntax $bat_config "GitHub" "Nord"
+auto_theme_tool_syntax $bat_config "OneHalfLight" "OneHalfDark"
 
 # FD-FIND
 zturbo \
@@ -52,20 +53,59 @@ zturbo \
 
 zinit pack"bgn-binary" for fzf
 
-# Enable synthax auto-theming support
+## Enable synthax auto-theming support
+
+# OneHalfLight
+local color00='#fafafa'
+local color01='#f0f0f1'
+local color02='#e5e5e6'
+local color03='#a0a1a7'
+local color04='#696c77'
+local color05='#383a42'
+local color06='#202227'
+local color07='#090a0b'
+local color08='#ca1243'
+local color09='#d75f00'
+local color0A='#c18401'
+local color0B='#50a14f'
+local color0C='#0184bc'
+local color0D='#4078f2'
+local color0E='#a626a4'
+local color0F='#986801'
+
 local lightFZF="\
-    --color=bg+:#f9f1eb,bg:#ffffff,spinner:#dc8a78,hl:#d20f39 \
-    --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
-    --color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#e36209 \
+    --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D \
+    --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C \
+    --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D \
     --ansi"
 
+# OneHalfDark
+local color00='#282c34'
+local color01='#353b45'
+local color02='#3e4451'
+local color03='#545862'
+local color04='#565c64'
+local color05='#abb2bf'
+local color06='#b6bdca'
+local color07='#c8ccd4'
+local color08='#e06c75'
+local color09='#d19a66'
+local color0A='#e5c07b'
+local color0B='#98c379'
+local color0C='#56b6c2'
+local color0D='#61afef'
+local color0E='#c678dd'
+local color0F='#be5046'
+
 local darkFZF="\
-    --color=bg+:#3B4252,bg:#24292e,spinner:#81A1C1,hl:#616E88 \
-    --color=fg:#D8DEE9,header:#616E88,info:#81A1C1 \
-    --color=pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9 \
-    --color=prompt:#81A1C1,hl+:#81A1C1 --ansi"
+    --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D \
+    --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C \
+    --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D \
+    --ansi"
 
 auto_theme_fzf $lightFZF $darkFZF
+
+##
 
 # TOPGRADE
 zturbo \
@@ -84,4 +124,4 @@ zturbo \
 
 # Enable synthax auto-theming support
 local git_config=$HOME/dotfiles/git/.gitconfig
-auto_theme_tool_syntax $git_config "GitHub" "Nord"
+auto_theme_tool_syntax $git_config "OneHalfLight" "OneHalfDark"
